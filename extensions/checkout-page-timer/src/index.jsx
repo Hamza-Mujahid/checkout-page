30b82faa6aabef7abe68286f97676af2cd7424c6
Hamza-Mujahid <80825408+Hamza-Mujahid@users.noreply.github.com>
2023-06-16 11:54:34 +0500

    customised timer

--- extensions/checkout-page-timer/src/index.jsx
+++ extensions/checkout-page-timer/src/index.jsx
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   render,
   BlockStack,
-  TextBlock,
-  useSettings,
-  View,
   Text,
+  useSettings,
+  View
 } from '@shopify/checkout-ui-extensions-react';
 
 render('Checkout::Dynamic::Render', () => <App />);
 
 function App() {
-  const {timer_before_text, timer_after_text, timer} = useSettings();
-  const {timer_text_color, timer_text_size, timer_size, timer_color} = useSettings()
-  const initialSeconds = timer * 60; // 15 minutes in seconds
-  const [seconds, setSeconds] = React.useState(initialSeconds);
+  const {
+    timer_before_text,
+    timer_after_text,
+    timer,
+    timer_ends,
+    timer_text_color,
+    timer_text_size,
+    timer_size,
+    timer_color
+  } = useSettings();
 
-  React.useEffect(() => {
+  const calculateEndTime = (timer) => {
+    const currentTime = new Date();
+    const endTime = new Date(currentTime.getTime() + timer * 60000); // Convert minutes to milliseconds
+
+    return endTime;
+  };
+
+  const getRemainingTimeInSeconds = (endTime) => {
+    const currentTime = new Date();
+    const remainingTimeInSeconds = Math.floor((endTime - currentTime) / 1000);
+
+    return remainingTimeInSeconds >= 0 ? remainingTimeInSeconds : 0;
+  };
+
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+
+    const endTime = calculateEndTime(timer); // Set the timer to 15 minutes
+    setSeconds(getRemainingTimeInSeconds(endTime));
+
     const interval = setInterval(() => {
       setSeconds((prevSeconds) => {
         if (prevSeconds <= 0) {
@@ -38,65 +63,37 @@ function App() {
     return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
   }, [seconds]);
 
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds % 60;
 
   return (
-    
-    <BlockStack 
-    border={'base'}
-    borderRadius={'base'}
-    padding={'tight'}
-    spacing={'loose'}
-    blockAlignment={'center'}
-    cornerRadius={'loose'}
-    overflow='hidden'
-    inlineAlignment={'center'}>
-      <TextBlock>
-    <View>
-      <Text size={timer_text_size} appearance={timer_text_color} emphasis='bold'>{timer_before_text}</Text>
-      <Text size={timer_size} emphasis='bold' appearance={timer_color} > {minutes} : {remainingSeconds}</Text>
-      <Text size={timer_text_size} appearance={timer_text_color} emphasis='bold'> {timer_after_text}</Text></View>
-    </TextBlock>
+    <BlockStack
+      border="base"
+      borderRadius="base"
+      padding="tight"
+      spacing="loose"
+      blockAlignment="center"
+      cornerRadius="loose"
+      overflow="hidden"
+      inlineAlignment="center"
+    >
+      <View>
+        <Text size={timer_text_size} appearance={timer_text_color} emphasis="strong">
+          {timer_before_text}
+        </Text>
+        {seconds > 0 ? (
+          <Text size={timer_size} emphasis="strong" appearance={timer_color}>
+            {formattedTime}
+          </Text>
+        ) : (
+          <Text size={timer_size} appearance={timer_color} emphasis="strong">
+            {timer_ends}
+          </Text>
+        )}
+        <Text size={timer_text_size} appearance={timer_text_color} emphasis="strong">
+          {timer_after_text}
+        </Text>
+      </View>
     </BlockStack>
-    
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import {
-//   useExtensionApi,
-//   render,
-//   Banner,
-//   useTranslate,
-// } from '@shopify/checkout-ui-extensions-react';
-
-// render('Checkout::Dynamic::Render', () => <App />);
-
-// function App() {
-//   const {extensionPoint} = useExtensionApi();
-//   const translate = useTranslate();
-//   return (
-//     <Banner title="checkout-timer">
-//       {translate('welcome', {extensionPoint})}
-//     </Banner>
-//   );
-// }
